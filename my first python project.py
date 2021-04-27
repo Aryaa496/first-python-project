0d989a288d8f8362a62dccaefde4b473404f34f9
Aryaa496 <82282070+Aryaa496@users.noreply.github.com>
2021-04-27 17:12:34 +0530

    Add files via upload

---
+++ my first python project.py
@@ -0,0 +1,18 @@
+
+condition= True
+
+
+while(condition):
+    
+    
+    student_info=input("enter the student information")
+    check=input("enter yes of u want to enter mkre student information or enter no")\
+        
+if check==yes:
+    condition=True
+elif check==no:
+    condition=False
+    
+     info=input("enter the student information")
+     print(info,type(info))  
+      
